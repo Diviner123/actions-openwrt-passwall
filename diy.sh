6288bc463c4b7ae3dbd7f601efa7bdb0ae234bf4
Diviner123 <wwwliuzhengzhi@163.com>
2020-03-07 16:01:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,20 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/0.10.10.1/g' package/base-files/files/bin/config_generate
+
+# OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# luci-app-k3screenctrl
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/luci-app-k3screenctrl
+
+# luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# wifi
+git clone https://github.com/Diviner123/phicommk3-firmware.git  package/phicommk3-firmware
+cd /package/phicommk3-firmware
+vm -f brcmfmac4366c-pcie.bin.ac88u /package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
